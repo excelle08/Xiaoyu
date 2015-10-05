2d5bfbb98e4d6a63dcfb76698a28b1298e3583e7
excelle08 <wsu@birchpress.com>
2015-10-05 10:28:33 +0800

    statistics

---
+++ www/api/statistics.py
@@ -0,0 +1,20 @@
+from model import PageView, db
+import time
+
+
+def get_pageviews(later_than):
+    return PageView.query.filter(time >= later_than).all()
+
+
+def get_pv_counts(later_than):
+    return {"pv": PageView.query.filter(time >= later_than).count()}
+
+
+def pageview(uid, ip, path):
+    p = PageView()
+    p.user = uid
+    p.ip_addr = ip
+    p.path = path
+
+    db.session.add(p)
+    db.session.commit()
