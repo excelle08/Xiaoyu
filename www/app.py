21dc10fa89c5ccd70340190362840231f01d7e81
Chion Tang <sdspeedonion@gmail.com>
2015-12-06 17:57:18 +0800

    Add run_app method

--- www/app.py
+++ www/app.py
@@ -14,8 +14,11 @@ def get_mysql_conn_str():
     return 'mysql+mysqlconnector://' + db_user + ':' + db_pass + '@' + db_host + ':' + str(db_port) + '/' + db_name
 
 
-if __name__=='__main__':
+def run_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = get_mysql_conn_str()
     app.config.from_object('config.config')
     app.jinja_env.filters['datetime'] = datetime_filter
     app.run(debug=False, host='0.0.0.0')
+
+if __name__=='__main__':
+    run_app()
